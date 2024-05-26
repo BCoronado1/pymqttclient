8ab81155fdf8438244ad9228176ed1a7e5af4fae
Braulio Coronado <bcoronado1@gmail.com>
2024-05-26 13:59:25 -0700

    Add cleanup method

--- pymqttclient/client.py
+++ pymqttclient/client.py
@@ -25,19 +25,31 @@ class MQTTClient:
             self.subscriptions = {"#"}
         self.client: mqtt.Client = mqtt.Client(client_id=self.client_id,
                                                callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
-        threading.Thread(daemon=True, target=self.connect).start()
+        self.is_running: bool = True
+        threading.Thread(daemon=True, target=self._connect).start()
+        time.sleep(0.1)  # Add a sleep to allow the paho-mqtt library to connect
         logging.info(f"MQTTClient {self.client_id} configured with address {self.host}:{self.port} "
                      f"reconnect_delay: {self.reconnect_delay}s "
                      f"reconnect_msg_interval: {self.reconnect_msg_interval} "
                      f"subscriptions: {self.subscriptions}.")
 
-    def connect(self):
+    def publish(self, topic: str, payload: bytes):
+        self.client.publish(topic=topic, payload=payload)
+
+    def publish_dict(self, topic: str, payload_dict: Dict):
+        self.publish(topic=topic, payload=json.dumps(payload_dict).encode())
+
+    def cleanup(self):
+        self.is_running = False
+        self.client.disconnect()
+
+    def _connect(self):
         is_connected: bool = False
         num_retries = 1
-        self.client.on_connect = self.on_connect
-        self.client.on_message = self.on_message
-        self.client.on_disconnect = self.on_disconnect
-        while not is_connected:
+        self.client.on_connect = self._on_connect
+        self.client.on_message = self._on_message
+        self.client.on_disconnect = self._on_disconnect
+        while self.is_running and not is_connected:
             try:
                 self.client.connect(host=self.host, port=self.port)
                 self.client.loop_start()
@@ -49,26 +61,21 @@ class MQTTClient:
                 num_retries += 1
                 time.sleep(self.reconnect_delay)
 
-    def publish(self, topic: str, payload: bytes):
-        self.client.publish(topic=topic, payload=payload)
-
-    def publish_dict(self, topic: str, payload_dict: Dict):
-        self.publish(topic=topic, payload=json.dumps(payload_dict).encode())
-
-    def on_message(self, client: mqtt.Client, userdata: Any, msg: mqtt.MQTTMessage):
+    def _on_message(self, client: mqtt.Client, userdata: Any, msg: mqtt.MQTTMessage):
         logging.debug(f"MQTTClient {self.client_id} received message. topic: {msg.topic} payload: {msg.payload}")
         for callback in self.callbacks:
             callback(topic=msg.topic, payload=msg.payload)
 
-    def on_connect(self, client: mqtt.Client, userdata: Any, connect_flags: mqtt.ConnectFlags,
-                   reason_code: mqtt.ReasonCode, properties: mqtt.Properties) -> None:
-        logging.info(f"MQTTClient {self.client_id} connected to {self.host}:{self.port}.")
+    def _on_connect(self, client: mqtt.Client, userdata: Any, connect_flags: mqtt.ConnectFlags,
+                    reason_code: mqtt.ReasonCode, properties: mqtt.Properties) -> None:
         for sub in self.subscriptions:
             self.client.subscribe(topic=sub)
+        logging.info(f"MQTTClient {self.client_id} connected to {self.host}:{self.port}. "
+                     f"Subscriptions: {self.subscriptions}.")
 
-    def on_disconnect(self, client: mqtt.Client, userdata: any, disconnect_flags: mqtt.DisconnectFlags,
-                      reason_code: mqtt.ReasonCode, properties: mqtt.Properties):
+    def _on_disconnect(self, client: mqtt.Client, userdata: any, disconnect_flags: mqtt.DisconnectFlags,
+                       reason_code: mqtt.ReasonCode, properties: mqtt.Properties):
         logging.info(f"MQTTClient {self.client_id} disconnected. Attempting reconnect...")
-        while not self.client.is_connected():
+        while self.is_running and not self.client.is_connected():
             self.client.reconnect()
             time.sleep(1)
